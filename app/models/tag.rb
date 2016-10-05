aed67ff75ccead1c73d9b636a9c8bf9d7ac28366
Raghu Betina <raghu@firstdraft.com>
2016-10-05 11:33:14 +0000

    Direct association between tagtorestaurants and tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,9 @@
 class Tag < ApplicationRecord
   # Direct associations
 
+  belongs_to :wordtag,
+             :class_name => "Tagtorestaurant"
+
   belongs_to :user
 
   # Indirect associations
--- app/models/tagtorestaurant.rb
+++ app/models/tagtorestaurant.rb
@@ -1,6 +1,10 @@
 class Tagtorestaurant < ApplicationRecord
   # Direct associations
 
+  has_one    :tag,
+             :foreign_key => "wordtag",
+             :dependent => :destroy
+
   belongs_to :restaurant
 
   # Indirect associations
